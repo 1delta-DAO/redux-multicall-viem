93ca342fa0ba28650db1e51329919c8b45ff4bd7
Achthar <69119535+Achthar@users.noreply.github.com>
2024-08-12 22:52:09 +0200

    fix method args

--- src/validation.ts
+++ src/validation.ts
@@ -2,7 +2,7 @@ export type MethodArg = string | number | BigInt
 export type MethodArgs = Array<MethodArg | MethodArg[]>
 
 export function isMethodArg(x: unknown): x is MethodArg {
-  return typeof BigInt(x as any) === 'bigint' || ['string', 'number'].indexOf(typeof x) !== -1
+  return (x === null || x === undefined) ? false : typeof BigInt(x as any) === 'bigint' || ['string', 'number'].indexOf(typeof x) !== -1
 }
 
 export function isValidMethodArgs(x: unknown): x is MethodArgs | undefined {
